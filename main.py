0943cb0cf0e0536b32535bcee4f74de74381af77
joshyliz <joshypons07@gmail.com>
2022-11-12 19:45:48 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
+import keyboard
+from time import sleep
+from winsound import Beep
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+sessions = AudioUtilities.GetAllSessions()
+
+def LowerVolume(sessions):
+    Beep(1500, 250)
+    for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name() == "Discord.exe":
+                volume.SetMasterVolume(0.15, None) 
+
+def IncreaseVolume(sessions):
+    Beep(500, 250)
+    for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name() == "Discord.exe":
+                volume.SetMasterVolume(1, None) 
+
+
+lowered = False
+pressed = False
+key = 'l'
+
+while(True):
+        if keyboard.is_pressed(key) != True:
+            pressed = False
+        elif keyboard.is_pressed(key) == True and lowered == False and pressed == False:
+            LowerVolume(sessions)
+            lowered = True
+            pressed = True
+        elif keyboard.is_pressed(key) == True and lowered == True and pressed == False:
+            IncreaseVolume(sessions)
+            lowered = False
+            pressed = True 
+        print("Volume Lowered =" ,lowered)
+
+
+       
